e4b3d081acd9cd0136a777eba1b95fd577334ddc
BdNWoG <billy.gao@hotmail.com>
2024-06-28 11:11:55 -0700

    auth

--- buddy/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ buddy/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,36 @@
-const SignInPage = () => {
-    return (
-        <div>
-            Sign In Page
-        </div>
-    )
-}
+import Image from "next/image";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
-export default SignInPage
+export default function Page() {
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col  items-center justify-center px-4">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2e2a47]">
+                        Welcome Back!
+                    </h1>
+                    <p className="text-base text-[#7e8ca0]">
+                        To get started, log in to or create  your account.
+                    </p>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignIn path="/sign-in"/>
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground" />
+                    </ClerkLoading>
+                </div>
+            </div>
+            <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+                <Image
+                    src="/buddy.svg"
+                    alt="signin"
+                    width={200}
+                    height={200}
+                />
+            </div>
+        </div>
+    );
+}
--- buddy/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ buddy/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,36 @@
-const SignUpPage = () => {
-    return (
-        <div>
-            Sign Up Page
-        </div>
-    )
-}
+import Image from "next/image";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
-export default SignUpPage
+export default function Page() {
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col  items-center justify-center px-4">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2e2a47]">
+                        Welcome Back!
+                    </h1>
+                    <p className="text-base text-[#7e8ca0]">
+                        To get started, log in to or create  your account.
+                    </p>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignUp path="/sign-up"/>
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground" />
+                    </ClerkLoading>
+                </div>
+            </div>
+            <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+                <Image
+                    src="/buddy.svg"
+                    alt="signin"
+                    width={200}
+                    height={200}
+                />
+            </div>
+        </div>
+    );
+}
