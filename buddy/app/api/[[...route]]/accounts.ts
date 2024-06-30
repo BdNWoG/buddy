5febc24181d1c318c65a658929dd50f59c5d0141
BdNWoG <billy.gao@hotmail.com>
2024-06-29 22:05:04 -0700

    Update accounts.ts

--- buddy/app/api/[[...route]]/accounts.ts
+++ buddy/app/api/[[...route]]/accounts.ts
@@ -4,6 +4,7 @@ import { db } from '@/db/drizzle';
 import { accounts } from '@/db/schema';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { HTTPException } from 'hono/http-exception'; 
+import { eq } from 'drizzle-orm';
 
 const app = new Hono()
     .get('/', clerkMiddleware(), async (c) => {
@@ -19,6 +20,7 @@ const app = new Hono()
                 name: accounts.name,
             })
             .from(accounts)
+            .where(eq(accounts.userId, auth?.userId));
 
         return c.json({ data });
     });
