742d9f28869dfb66fbbc32a786edf694e2cb63dc
BdNWoG <billy.gao@hotmail.com>
2024-07-06 16:28:20 -0700

    schema

--- buddy/db/schema.ts
+++ buddy/db/schema.ts
@@ -1,6 +1,7 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core"
 import { createInsertSchema } from "drizzle-zod"
 import { relations } from "drizzle-orm"
+import { z } from "zod"
 
 export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
@@ -9,7 +10,9 @@ export const accounts = pgTable("accounts", {
     userId: text("user_id").notNull(),
 })
 
-export const accountsRelations
+export const accountsRelations = relations(accounts, ({ many }) => ({
+    transactions: many(transactions)
+}))
 
 export const insertAccountSchema = createInsertSchema(accounts)
 
@@ -19,6 +22,10 @@ export const categories  = pgTable("categories", {
     name: text("name").notNull(),
     userId: text("user_id").notNull(),
 })
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+    transactions: many(transactions)
+}))
 
 export const insertCategorySchema = createInsertSchema(categories)
 
@@ -35,3 +42,18 @@ export const transactions = pgTable("transactions", {
         onDelete: "set null",
     }),
 })
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+    accounts: one(accounts, {
+        fields: [transactions.accountId], 
+        references: [accounts.id]
+    }),
+    categories: one(categories, {
+        fields: [transactions.categoryId], 
+        references: [categories.id]
+    }),
+})) 
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+    date: z.coerce.date()
+});
